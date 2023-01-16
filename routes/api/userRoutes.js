ac61a1cf26544d43264fd4b85688becbd16bcc91
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-01-16 16:38:12 +1030

    Updated userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 const {
-  getSinglePost,
-  getPosts,
-  createPost,
+  getUsers,
+  getUserById,
+  NewUser,
+  updateUser,
+  removeUser,
+  addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
 
-router.route('/').get(getPosts).post(createPost);
-
-router.route('/:postId').get(getSinglePost);
+router.route('/').get(getUsers).post(NewUser);
+router.route ('/:userId').get(getUserById).put(updateUser).delete(removeUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
  
